e5b3670833fd9036e443f7344869defe9317de7e
itsandromeda <itsandromeda@gmail.com>
2017-04-23 22:03:30 -0500

    Notes - reactions

--- assets/js/app.js
+++ assets/js/app.js
@@ -82,3 +82,71 @@ function navigationOptions(container, icon, description, link) {
 for (i = 0; i < navOptions.length; i += 1) {
     navigationOptions(containerList[i], iconList[i], descriptionList[i], linkList[i]);
 }
+
+/*REACTION BOX*/
+
+var reactions = [{
+    container: "reaction-box",
+    icon: "fa-user-o",
+    title: "New followers",
+    date: "today",
+    count: "252"
+}, {
+    container: "reaction-box",
+    icon: "fa-heart-o",
+    title: "New likes",
+    date: "today",
+    count: "930"
+}, {
+    container: "reaction-box",
+    icon: "fa-sticky-note-o",
+    title: "New notes",
+    date: "today",
+    count: "1020"
+}, {
+    container: "reaction-box",
+    icon: "fa-comment-o",
+    title: "New comments",
+    date: "today",
+    count: "210"
+}, {
+    container: "reaction-box",
+    icon: "fa-user-o",
+    title: "New messages",
+    date: "last 7 days",
+    count: "317"
+}];
+
+var containerArray = reactions.map((e) => e.container),
+    iconArray = reactions.map((e) => e.icon),
+    titleArray = reactions.map((e) => e.title),
+    descriptionArray = reactions.map((e) => e.date),
+    linkArray = reactions.map((e) => e.count),
+    y;
+
+function reaction(container, icon, title, date, count) {
+    var parent = document.createElement("div"),
+        iconE = document.createElement("i"),
+        textA = document.createElement("span"),
+        textB = document.createElement("span"),
+        textC = document.createElement("span"),
+        titleNode = document.createTextNode(title),
+        dateNode = document.createTextNode(date),
+        countNode = document.createTextNode(count);
+
+    parent.classList.add("reactions");
+    iconE.classList.add("fa");
+    iconE.classList.add(icon);
+    parent.appendChild(iconE);
+    textA.appendChild(titleNode);
+    textB.appendChild(dateNode);
+    textC.appendChild(countNode);
+    parent.appendChild(textA);
+    parent.appendChild(textB);
+    parent.appendChild(textC);
+    document.getElementById(container).appendChild(parent);
+}
+
+for (i = 0; i < reactions.length; i += 1) {
+    reaction(containerArray[i], iconArray[i], titleArray[i], descriptionArray[i], linkArray[i]);
+}
